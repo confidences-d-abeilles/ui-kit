56590348628d4f142fa18798b2a0da26c55ffc30
cchampou <clement@champouillon.com>
2019-06-26 00:37:26 +0200

    Lerna upgrade

---
+++ packages/button/lib/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _default = function _default() {
+  return _react["default"].createElement("p", null, "Test2");
+};
+
+exports["default"] = _default;
