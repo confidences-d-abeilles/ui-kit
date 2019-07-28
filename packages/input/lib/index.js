db4c1c60159f7e361329865b35a1dbe8e73d11f3
cchampou <clement@champouillon.com>
2019-07-28 14:19:44 +0200

    Added input packages

---
+++ packages/input/lib/index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _styled = _interopRequireDefault(require("@emotion/styled"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _templateObject() {
+  var data = _taggedTemplateLiteral(["\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+var StyledInput = (0, _styled["default"])('input')(_templateObject());
+var _default = StyledInput;
+exports["default"] = _default;
---
+++ packages/input/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const StyledInput = styled('input')`
+`;
+
+export default StyledInput;
