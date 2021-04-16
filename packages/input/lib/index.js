53101e8b88d6097a30c08534c875ddd3bf961853
Clément Champouillon <clement@champouillon.com>
2021-04-16 17:03:26 +0200

    chore(deps): upgrade vulnerable packages (#15)

--- packages/input/lib/index.js
+++ packages/input/lib/index.js
@@ -9,21 +9,13 @@ var _react = _interopRequireDefault(require("react"));
 
 var _styled = _interopRequireDefault(require("@emotion/styled"));
 
+var _templateObject;
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n  &[type=\"text\"], &[type=\"email\"], &[type=\"password\"], &[type=\"tel\"], &[type=\"number\"] {\n    width: calc(100% - 2rem);\n    padding: 0.25rem 0.5rem;\n    margin: 0.5rem;\n    border-style: solid;\n    border-color: ", ";\n    border-width: 0 0 1px 0;\n    background: none;\n    &:focus {\n      margin-bottom: calc(0.5rem - 1px);\n      border-width: 0 0 2px 0;\n    }\n    font-size: 1rem;\n  }\n"]);
-
-  _templateObject = function _templateObject() {
-    return data;
-  };
-
-  return data;
-}
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var StyledInput = (0, _styled["default"])('input')(_templateObject(), function (_ref) {
+var StyledInput = (0, _styled["default"])('input')(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n  &[type=\"text\"], &[type=\"email\"], &[type=\"password\"], &[type=\"tel\"], &[type=\"number\"] {\n    width: calc(100% - 2rem);\n    padding: 0.25rem 0.5rem;\n    margin: 0.5rem;\n    border-style: solid;\n    border-color: ", ";\n    border-width: 0 0 1px 0;\n    background: none;\n    &:focus {\n      margin-bottom: calc(0.5rem - 1px);\n      border-width: 0 0 2px 0;\n    }\n    font-size: 1rem;\n  }\n"])), function (_ref) {
   var theme = _ref.theme;
   return theme.colors.black;
 });
--- packages/top-bar/lib/index.js
+++ packages/top-bar/lib/index.js
@@ -7,20 +7,12 @@ exports["default"] = void 0;
 
 var _styled = _interopRequireDefault(require("@emotion/styled"));
 
+var _templateObject;
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n  width: 100%;\n  text-align: center;\n  padding: 0.25rem 0.5rem;\n  font-size: 0.8rem;\n  color: white;\n  background-color: black;\n"]);
-
-  _templateObject = function _templateObject() {
-    return data;
-  };
-
-  return data;
-}
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var _default = (0, _styled["default"])('div')(_templateObject());
+var _default = (0, _styled["default"])('div')(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n  width: 100%;\n  text-align: center;\n  padding: 0.25rem 0.5rem;\n  font-size: 0.8rem;\n  color: white;\n  background-color: black;\n"])));
 
 exports["default"] = _default;
